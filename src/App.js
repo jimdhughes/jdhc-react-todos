3240b70eeecdf9088a3d821868bc93335846025e
James Hughes <jim.d.hughes@gmail.com>
2019-04-09 21:04:08 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import TodoList from './components/TodoList';
+import { StateProvider } from './context/StateProvider';
+import TodoStore from './store/TodoStore';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [state, setState] = useState(null);
+  let reducer = (state, action) => {
+    let idx, todos, todo;
+    switch (action.type) {
+      case 'ADD_TODO':
+        todo = action.payload;
+        todo.isCompleted = false;
+        return {
+          ...state,
+          todos: [...state.todos, todo]
+        };
+      case 'DELETE_TODO':
+        todos = state.todos;
+        idx = todos.indexOf(action.payload);
+        if (idx > -1) {
+          todos.splice(idx, 1);
+        }
+        return { ...state, todos };
+      case 'TOGGLE_TODO':
+        todos = state.todos;
+        idx = todos.indexOf(action.payload);
+        if (idx > -1) {
+          todos[idx].isCompleted = !todos[idx].isCompleted;
+          todos[idx].completedAt = todos[idx].isCompleted
+            ? (todos[idx].completedAt = new Date())
+            : null;
+        }
+        return { ...state, todos };
+      default:
+        break;
+    }
+  };
+
+  useEffect(() => {
+    async function loadState() {
+      let t = await TodoStore.todos.toArray();
+      setState({ todos: t });
+    }
+    loadState();
+  }, []);
+
+  return state ? (
+    <StateProvider initialState={state} reducer={reducer}>
+      <TodoList />
+    </StateProvider>
+  ) : (
+    <div>Loading...</div>
+  );
+};
 
 export default App;
